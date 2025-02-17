90dbc2e570d66c00f3f40ec2300dd070871d58de
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-02-17 19:18:12 -0300

    carrousel 1

---
+++ front/src/components/DestacadoImagenes/DestacadoImagenes.tsx
@@ -0,0 +1,55 @@
+"use client"
+import Image from "next/image";
+import { IDestacadoPics } from "../Section-1/Section-1";
+import styles from "./DestacadoImagenes.module.css";
+import { useState } from "react";
+
+interface Props {
+  destacadoPics: IDestacadoPics[];
+}
+
+const DestacadoImagenes = ({ destacadoPics }:any) => {
+    const [index, setIndex] = useState(0);
+  
+    const nextSlide = () => {
+      if (index < destacadoPics.length - 5) {
+        setIndex(index + 1);
+      }
+    };
+  
+    const prevSlide = () => {
+      if (index > 0) {
+        setIndex(index - 1);
+      }
+    };
+  
+    return (
+      <div className={styles.carouselContainer}>
+        <button className={styles.arrowLeft} onClick={prevSlide}>&#9664;</button>
+        <div className={styles.carouselWrapper}>
+          <div
+            className={styles.carouselInner}
+            style={{ transform: `translateX(-${index * 22}%)`, transition: "transform 0.5s ease-in-out" }}
+          >
+            {destacadoPics.map((item:any) => (
+              <div className={styles.slide} key={item.id}>
+                <Image
+                  className={styles.imagenDestacada}
+                  width={500}
+                  height={500}
+                  src={item.imagen}
+                  alt="zapatilla"
+                />
+                <p className={styles.nombre}>{item.nombre}</p>
+                <p className={styles.genero}>{item.genero}</p>
+                <p className={styles.precio}>{item.precio}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className={styles.arrowRight} onClick={nextSlide}>&#9654;</button>
+      </div>
+    );
+  };
+  
+  export default DestacadoImagenes;
--- front/src/components/Section-1/Section-1.tsx
+++ front/src/components/Section-1/Section-1.tsx
@@ -1,18 +1,58 @@
-import styles from "./Section-1.module.css"
-import jordan from "../../../assets/jordan.jpg"
-import Image from "next/image";
+import styles from "./Section-1.module.css";
+import jordan from "../../../assets/jordan.jpg";
+import Image, { StaticImageData } from "next/image";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import DestacadoImagenes from "../DestacadoImagenes/DestacadoImagenes";
+
+export interface IDestacadoPics{
+    imagen:StaticImageData,
+    id:number
+}
+
+const destacadoPics:IDestacadoPics[] = [
+  {
+    imagen: jordan,
+    id: 1,
+  },
+  {
+    imagen: jordan,
+    id: 2,
+  },
+  {
+    imagen: jordan,
+    id: 3,
+  },
+  {
+    imagen: jordan,
+    id: 5,
+  },
+  {
+    imagen: jordan,
+    id: 4,
+  },
+  {
+    imagen: jordan,
+    id: 6,
+  },
+  {
+    imagen: jordan,
+    id: 7,
+  },
+];
 
 const Section1 = () => {
-    return(
-        <div>
-            <div>
-                <h3>Destacado</h3>
-            </div>
-            <div>
-                <Image src={jordan} alt="zapatilla jordan" ></Image>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.containerSection1}>
+      <div >
+        <h3 style={{fontSize:"20px",marginBottom:"3vw",marginTop:"2vw",marginLeft:"2vw"}}>Destacado</h3>
+      </div>
+        <DestacadoImagenes destacadoPics={destacadoPics} />
+    </div>
+  );
 };
 
 export default Section1;
