7c99d2dd48acb418b45b4ea4db2528d3c67eb0d3
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-03-24 20:59:03 -0300

    vercerl error

--- front/src/utils/supabase/server.ts
+++ front/src/utils/supabase/server.ts
@@ -5,8 +5,10 @@ export const createClient = async () => {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL! ||
+      "https://tibzmcuyztupkpqlrkfu.supabase.co/",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! ||
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpYnptY3V5enR1cGtwcWxya2Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNjUxMTYsImV4cCI6MjA1Nzg0MTExNn0.xrFVXdYs-18gn9AHmbzE1tnqNeyARQta7VvVZNAu5uI",
     {
       cookies: {
         getAll() {
@@ -24,6 +26,6 @@ export const createClient = async () => {
           }
         },
       },
-    },
+    }
   );
 };
