d4a3a1fc2cca77176eef23a3e084060b349f333c
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-03-24 15:05:42 -0300

    back again

--- front/src/components/Section-1/Section-1.tsx
+++ front/src/components/Section-1/Section-1.tsx
@@ -1,18 +1,7 @@
 import styles from "./Section-1.module.css";
-import { StaticImageData } from "next/image";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import LoMasDestacado from "../LoMasDestacado/LoMasDestacado";
 import { Suspense } from "react";
 
-//width: 1080px i need a change
-
-export interface IDestacadoPics {
-  imagen: StaticImageData;
-  id: number;
-  nombre: string;
-}
 
 const Section1 = () => {
   return (
