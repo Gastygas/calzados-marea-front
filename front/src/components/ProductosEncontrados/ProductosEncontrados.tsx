a46f972bf0732bfa53030c138bcb05832b35af19
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-03-24 15:14:20 -0300

    back again

--- front/src/components/ProductosEncontrados/ProductosEncontrados.tsx
+++ front/src/components/ProductosEncontrados/ProductosEncontrados.tsx
@@ -1,34 +1,63 @@
+"use client"
 import Link from "next/link";
-import { IDestacadoPics } from "../Section-1/Section-1";
 import styles from "./ProductosEncotrados.module.css"
 import Image from "next/image";
+import { IZapatilla } from "@/helpers/interfaces";
+import { useEffect, useState } from "react";
+import { FindDestacadosAction } from "@/actions/zapatillas.actions";
 
-interface Props {
-  productos: IDestacadoPics[];
-}
 
-const ProductosEncontrados = ({productos}: Props) => {
+const ProductosEncontrados = () => {
+
+const [zapatillaEncontradas, setZapatillasEncontradas] = useState<
+      IZapatilla[] | []
+    >([
+      {
+        nombre: "none",
+        id: "none",
+        precio: "none",
+        marca: "nike",
+        talle: ["none"],
+        color: "none",
+        destacado: false,
+        nuevo: false,
+        fotos: ["https://none.jpg"],
+        genero: "none",
+      },
+    ]);
+  
+    useEffect(() => {
+      const getZapatillas = async () => {
+        const zapatillas: IZapatilla[] | null = await FindDestacadosAction();
+  
+        if (zapatillas !== null) {
+          setZapatillasEncontradas(zapatillas);
+        }
+      };
+  
+      getZapatillas();
+    }, []);
+
     return (
         <div className={styles.productosContainer}>
-            {productos.map((item: any) => (
+            {zapatillaEncontradas.map((item: any) => (
               <div className={styles.slide} key={item.id}>
                 <Link href={`/calzado-${item.nombre}`}>
                   <Image
                     className={styles.imagenDestacada}
                     width={100}
-                    src={item.imagen}
+                    height={100}
+                    src={item.fotos[0]}
                     alt="zapatilla"
                   />
                   <div className={styles.infoZapas}>
-                    <p className={styles.nombre}>{item.nombre}</p>
-                    <p className={styles.genero}>
-                      {item.genero
-                        ? item.genero
-                        : "zapatillas jordan para hombre"}
-                    </p>
-                    <p className={styles.precio}>
-                      $ {item.precio ? item.precio : "120.000"}
-                    </p>
+                  <p className={styles.nombre}>{item.nombre}</p>
+                      <p className={styles.genero}>
+                        {item.genero === "unisex"
+                          ? `zapatillas ${item.marca} unisex`
+                          : `zapatillas ${item.marca} para ${item.genero}`}
+                      </p>
+                      <p className={styles.precio}>$ {item.precio}</p>
                   </div>
                 </Link>
               </div>
